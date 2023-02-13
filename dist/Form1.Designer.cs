e58f9bea35f9da76d60b96c3ffc59a7d164fdb56
vbif fdlei <qwertysdfg77@gmail.com>
2023-02-13 04:09:58 +0300

    Добавьте файлы проекта.

---
+++ dist/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace dist
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.svToken = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // svToken
+            // 
+            this.svToken.Location = new System.Drawing.Point(184, 219);
+            this.svToken.Multiline = true;
+            this.svToken.Name = "svToken";
+            this.svToken.Size = new System.Drawing.Size(367, 18);
+            this.svToken.TabIndex = 1;
+            this.svToken.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(339, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(393, 159);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(324, 298);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(557, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 37);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Save ID-токен";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.svToken);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox svToken;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ dist/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YandexDisk.Client.Clients;
+using YandexDisk.Client.Http;
+using YandexDisk.Client.Protocol;
+
+namespace dist
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";         
+        }
+         private async void YanDis()
+        {
+
+            try
+            {
+               var api = new DiskHttpApi(GetToken());
+               var rootFolderData = await api.MetaInfo.GetInfoAsync(new ResourceRequest
+                {
+                    Path = "/"
+
+                });
+
+               foreach (var item in rootFolderData.Embedded.Items)
+
+                {
+                    textBox2.Text += (($"{item.Name}\t{item.Type}\t{item.MimeType}")) + Environment.NewLine;
+
+                }
+
+            }
+            catch 
+            {
+                MessageBox.Show("whats wrong");
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private async void SaveToken()
+        {
+            string path = (Directory.GetCurrentDirectory() + @"\Feel\config.JSON");
+            var token = svToken.Text;
+            if (token != "")
+                using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+                {
+                    byte[] buffer = Encoding.Default.GetBytes(svToken.Text);
+                    await fs.WriteAsync(buffer, 0, buffer.Length);
+                }
+            else
+                MessageBox.Show("Введите свой ID-токен");
+
+        }
+        private  void CreatConfig()
+        {
+            string path = (Directory.GetCurrentDirectory() + @"\Feel");
+            if(!Directory.Exists(path))
+            Directory.CreateDirectory(path); 
+        }
+
+        private string GetToken()
+        {
+            string path = (Directory.GetCurrentDirectory() + @"\Feel\config.JSON");
+            if (File.Exists(path))
+                using (FileStream fs = File.OpenRead(path))
+                {
+                    byte[] buffer = new byte[fs.Length];
+                    fs.Read(buffer, 0, buffer.Length);
+                    string token = Encoding.Default.GetString(buffer);
+                    return token;
+                }
+            else 
+            {
+                MessageBox.Show("Save your ID-Token");
+                return null;
+            }
+                    
+                
+
+                
+        }
+        private  void button3_Click(object sender, EventArgs e)
+        {
+            YanDis();
+             
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CreatConfig();
+            SaveToken();
+        }
+    }
+}
